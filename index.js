b6365433d5deee1c125b61af51cb29a16e8bf96b
Kilchi555 <pascal_kilchenmann@icloud.com>
2024-04-19 14:05:40 +0200

    done

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ import {
 } from 'tw-elements';
 
 initTWE({ Collapse, Dropdown, Ripple, Carousel, Input });
+
+const form = document.querySelector('form');
+
+function sendEmail() {
+    Email.send({
+        Host : "mail.infomaniak.com",
+        Username : "info@laurasbeauty.ch",
+        Password : "fisLW95_2",
+        To : 'info@laurasbeauty.ch',
+        From : "info@laurasbeauty.ch",
+        Subject : "This is the subject",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    sendEmail();
+});
