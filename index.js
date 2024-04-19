81456026488168231e3cea31f0ce5d1f1591063b
Kilchi555 <pascal_kilchenmann@icloud.com>
2024-04-19 13:18:56 +0200

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+// Initialization for ES Users
+import {
+    Collapse,
+    Dropdown,
+    Ripple,
+    Carousel,
+    Input,
+    initTWE
+} from 'tw-elements';
+
+initTWE({ Collapse, Dropdown, Ripple, Carousel, Input });
